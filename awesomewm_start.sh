1563d029b10829fe9ed34329194d5c9cf06b5823
Frederik Braun <fbraun@mozilla.com>
2015-03-19 12:45:51 +0100

    remove some unneccessary apps from awesomewm autostart

--- awesomewm_start.sh
+++ awesomewm_start.sh
@@ -3,11 +3,8 @@ wmname LG3D
 setxkbmap de
 xfce4-power-manager
 update-notifier
-xscreensaver -nosplash
 pgrep nm-applet || nm-applet
 pgrep tomboy || tomboy
 autocutsel -f
 fixkeyboard
-workrave &          
-ubuntuone-launch &
 /bin/bash /usr/bin/VidyoDesktop &
